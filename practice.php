44e2463babfcf771d9b600cfb83719e5a7ef0257
Kana <chi.sk.924.f@gmail.com>
2020-03-28 09:22:15 +0000

    asignment

--- practice.php
+++ practice.php
@@ -18,4 +18,61 @@ $tech_boost ='tech';
 $tech_boost .=' boost';
 echo $tech_boost;
 echo "\n";
+echo "\n";
+
+$name ="Kana";
+switch ($name) {
+  case "Kana":
+    echo"私はKanaです。";
+    break;
+  default:
+    echo "私はKanaではありません。";
+    break;
+}
+echo "\n";
+echo "\n";
+
+$total = 0;
+echo $total;
+echo "\n";
+for ($i =0; $i <= 10000; $i++) {
+  $total += $i;
+}
+  echo $total;
+echo "\n";
+echo "\n";
+
+$fruits = array("西瓜","柿子","荔枝","李子","梨子");
+foreach($fruits as $fruit) {
+  echo "要素は" . $fruit;
+  echo "\n";
+}
+echo "\n";
+
+$question = "問4";
+echo $question;
+echo "\n";
+
+$answer1 ="・1行目のコメントのスラッシュの前に*が入っていない";
+echo $answer1;
+echo "\n";
+
+$answer2 ="・条件が100以下の数字の間で…ということなので、\n　<　ではなくて、<= にならないといけない";
+echo $answer2;
+echo "\n";
+
+$answer3 ="・10行目のセミコロンが抜けている";
+echo $answer3;
+echo "\n";
+echo "\n";
+
+$start = 1;
+$end = 100;
+for ($i = $start; $i <= $end; $i++) {
+  if($i % 5 == 0 ) {
+    echo $i;
+    echo "\n";
+  }
+}
+
 ?>
